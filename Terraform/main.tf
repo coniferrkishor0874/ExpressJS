7c2e6d6d642bef1315954f76a0b2be34834dda37
coniferrkishor0874 <rajkishor08reign@gmail.com>
2023-03-26 21:52:52 +0530

    aws-ecs.yaml file is updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -75,26 +75,6 @@ variable "ecr_repo_url" {}
 # }
 
 
-#resource "aws_iam_role" "ecs_task_role" {
-#   name = "ecs-task-role"
-#   assume_role_policy = jsonencode({ 
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ecs-tasks.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
-
-# resource "aws_iam_role_policy_attachment" "ecs_task_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-#   role       = aws_iam_role.ecs_task_role.name
-# }
 
 
 
